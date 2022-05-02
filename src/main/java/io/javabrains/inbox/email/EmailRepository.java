55a074d221cb13c65097b95154a581144f5a35c8
vaibhav-tomar <63999269+vaibhav-tomar@users.noreply.github.com>
2022-05-02 22:48:33 +0530

    final commit

---
+++ src/main/java/io/javabrains/inbox/email/EmailRepository.java
@@ -0,0 +1,8 @@
+package io.javabrains.inbox.email;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends CassandraRepository<Email, UUID> {
+}
---
+++ src/main/java/io/javabrains/inbox/email/EmailService.java
@@ -0,0 +1,55 @@
+package io.javabrains.inbox.email;
+
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.javabrains.inbox.emailslist.EmailListItem;
+import io.javabrains.inbox.emailslist.EmailListItemKey;
+import io.javabrains.inbox.emailslist.EmailListItemRepository;
+import io.javabrains.inbox.folders.UnreadEmailStatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmailService {
+
+    @Autowired private EmailRepository emailRepository;
+    @Autowired private EmailListItemRepository emailListItemRepository;
+    @Autowired private UnreadEmailStatsRepository unreadEmailStatsRepository;
+
+
+    public void sendEmail(String from, List<String> to, String subject, String body) {
+        Email email = new Email();
+        email.setTo(to);
+        email.setSubject(subject);
+        email.setFrom(from);
+        email.setBody(body);
+        email.setId(Uuids.timeBased());
+
+        emailRepository.save(email);
+
+        to.forEach(toId -> {
+            EmailListItem item = createEmailListItem(to, subject, email, toId, "Inbox");
+            emailListItemRepository.save(item);
+            unreadEmailStatsRepository.incrementUnreadCount(toId, "Inbox");
+        });
+
+        EmailListItem sentItemsEntry = createEmailListItem(to, subject, email, from, "Sent Items");
+        sentItemsEntry.setUnread(false);
+        emailListItemRepository.save(sentItemsEntry);
+    }
+
+    private EmailListItem createEmailListItem(List<String> to, String subject, Email email, String itemOwner, String folder) {
+        EmailListItemKey key = new EmailListItemKey();
+        key.setId(itemOwner);
+        key.setLabel(folder);
+        key.setTimeUUID(email.getId());
+        EmailListItem item = new EmailListItem();
+        item.setKey(key);
+        item.setTo(to);
+        item.setSubject(subject);
+        item.setUnread(true);
+        return item;
+
+    }
+}
